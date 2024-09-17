eabd6d9ed506ab25494907dec90d4081db12f1a5
Tipu Sultan <tipusultan.ctg@gmail.com>
2024-09-17 10:40:25 +0600

    updated

--- resources/views/sales/show.blade.php
+++ resources/views/sales/show.blade.php
@@ -152,12 +152,10 @@
             </div>
         </div>
     </div>
-    <div class="text-end no-print mt-3">
-        <button type="button" class="btn btn-primary" onclick="javascript:window.print();">
-            <!-- Download SVG icon from http://tabler-icons.io/i/printer -->
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2"></path><path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4"></path><path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z"></path></svg>
-            Print Invoice
-        </button>
+    <div class="text-center no-print mt-3">
+        <a href="{{ route('invoice.pdf', $sale->id) }}" class="btn btn-icon btn-danger">
+            <i class="fas fa-file-pdf"></i> Download PDF
+        </a>
     </div>
 
 @endsection
